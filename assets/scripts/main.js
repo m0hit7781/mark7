6b59f66ee5e581ca66a147ec7f5bba3ce29882fd
Mohit Kumar <mohitkr.7781@gmail.com>
2022-09-05 20:16:25 +0530

    first commit

---
+++ assets/scripts/main.js
@@ -0,0 +1,16 @@
+const userInput = document.querySelector("textarea");
+const button = document.querySelector("button");
+const translatedData = document.querySelector(".translated");
+
+button.addEventListener("click", () => {
+  const API = `https://api.funtranslations.com/translate/klingon.json?text=${userInput.value}`;
+  fetch(API)
+    .then((res) => res.json())
+    .then((data) => {
+      translatedData.innerHTML = data.contents.translated;
+    })
+    .catch((err) => {
+      alert("Something went error. Try again Later");
+      console.log(err);
+    });
+});
